09b7d59d3a9527a81d6758ba1cbd920fd2ef9114
junki-higuchi <you@example.com>
2018-02-11 08:56:38 +0000

    user registration

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -30,6 +30,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
+        protected $redirectTo = '/';
         $this->middleware('guest', ['except' => 'getLogout']);
     }
 
